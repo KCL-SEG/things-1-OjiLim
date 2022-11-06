cddb6c8ecba2418f30f75f6e0ee14ca664b7de0c
Ryan Lim (K21001230) <ryan.lim@kcl.ac.uk>
2022-11-06 00:22:39 +0000

    fix Things model error

--- things/models.py
+++ things/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.db.models.Model
+from django.db.models import Model
 
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.CharField()
+    name = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 60)
     quantity = models.IntegerField()
 
 
