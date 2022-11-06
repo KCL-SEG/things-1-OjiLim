256f211df7e6b052718570c63bf39888b2ea264b
Ryan Lim (K21001230) <ryan.lim@kcl.ac.uk>
2022-11-06 01:14:59 +0000

    add things migration

--- things/models.py
+++ things/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import Model
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class Thing(models.Model):
+class thing(models.Model):
     #random test
     name = models.CharField(max_length = 30, unique = True, blank = False)
     description = models.CharField(max_length = 120)
