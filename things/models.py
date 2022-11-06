0a2ac4c5ee899aab925a033ffff8a13f81ff1cc1
Ryan Lim (K21001230) <ryan.lim@kcl.ac.uk>
2022-11-06 00:18:27 +0000

    add Things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.Model
+
+class Thing(models.Model):
+    name = models.CharField()
+    description = models.CharField()
+    quantity = models.IntegerField()
+
 
 # Create your models here.
